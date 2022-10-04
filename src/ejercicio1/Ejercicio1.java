272d10a8585d5a0fe9311219b8075d19fa42d667
Antonio Navarro <anavarro41yt@gmail.com>
2022-10-04 11:01:38 +0200

    Mid

--- src/ejercicio1/Ejercicio1.java
+++ src/ejercicio1/Ejercicio1.java
@@ -1,5 +1,23 @@
 package ejercicio1;
 
+import java.util.Scanner;
+
 public class Ejercicio1 {
+    public static void main(String[] args) {
+        final Scanner read = new Scanner(System.in);
+
+        long num;
+
+        System.out.print("Introduce un número: ");
+        num = read.nextLong();
+
+        num %= 2;
+
+        if (num == 0){
+            
+        }
+
+        read.close();
+    }
     
 }
