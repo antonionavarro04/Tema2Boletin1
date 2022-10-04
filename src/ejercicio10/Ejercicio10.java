2a92f2839dd7a5c21f97c4c66eee07b3abbafa7a
navarro41yt <anavarro41yt@gmail.com>
2022-10-04 20:24:21 +0200

    Falta Ejercicio 10

---
+++ src/ejercicio10/Ejercicio10.java
@@ -0,0 +1,12 @@
+package ejercicio10;
+
+// ? Importamos el Scanner y Locale
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ejercicio10 {
+    public static void main(String[] args) {
+        
+        
+    }
+}
