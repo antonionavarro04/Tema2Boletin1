8ccde375834eadefc5bc4ef9d521faec677ac161
Antonio Navarro <anavarro41yt@gmail.com>
2022-10-04 12:27:47 +0200

    4 Update

--- src/ejercicio4/Ejercicio4.java
+++ src/ejercicio4/Ejercicio4.java
@@ -1,7 +1,20 @@
 package ejercicio4;
 
+// ? Importamos el Scanner y Locale
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Ejercicio4 {
     public static void main(String[] args) {
+        // ^ Creamos dos variables, num1 y num2 las cuales van a ser ordenadas de menor a mayor una vez el usuario introduzca sus valores
+
+        // ^ Creamos el Scanner, se lo asignamos a la variable read y cambiamos su formato al de US
+        final Scanner read = new Scanner(System.in);
+        read.useLocale(Locale.US);
+
+
         
+        // ^ Cerramos el Scanner
+        read.close();
     }
 }
