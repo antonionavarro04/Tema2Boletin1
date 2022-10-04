833d4fbfb239df91d721e6e4abc67cc777abc8fa
Antonio Navarro <anavarro41yt@gmail.com>
2022-10-04 12:05:15 +0200

    Master

--- src/ejercicio2/Ejercicio2.java
+++ src/ejercicio2/Ejercicio2.java
@@ -1,7 +1,35 @@
 package ejercicio2;
 
+// ? Importamos el Scanner y Locale
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Ejercicio2 {
     public static void main(String[] args) {
-        
+        // ^ Creamos dos Variables, una para el primer numero introducido y otra para el segundo
+        double num1, num2;
+
+        // ^ Creamos el Scanner, se lo asignamos a la variable read y cambiamos su formato al de US
+        final Scanner read = new Scanner(System.in);
+        read.useLocale(Locale.US);
+
+        // ! Pedimos al usuario que nos introduzca dos numeros para comprobar si son iguales posteriormente
+        System.out.print("Introduce un número: ");
+        num1 = read.nextDouble();
+
+        System.out.print("Introduce otro número: ");
+        num2 = read.nextDouble();
+
+        // ! Comprobamos mediante un if si son iguales los numeros e imprimimos el resultado por pantalla
+        if (num1 == num2){
+            System.out.println(num1 + " y " + num2 + " son iguales");
+        }
+
+        else{
+            System.out.println(num1 + " y " + num2 + " son diferentes");
+        }
+
+        // ^ Cerramos el Scanner
+        read.close();
     }
 }
