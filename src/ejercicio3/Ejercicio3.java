959deb9c3941a24224e6055dc9a423b16811b2ad
Antonio Navarro <anavarro41yt@gmail.com>
2022-10-04 12:20:26 +0200

    Ejercicio3 Finalizado

--- src/ejercicio3/Ejercicio3.java
+++ src/ejercicio3/Ejercicio3.java
@@ -1,7 +1,32 @@
 package ejercicio3;
 
+// ? Importamos el Scanner y Locale
+import java.util.Scanner;
+import java.util.Locale;
+
 public class Ejercicio3 {
     public static void main(String[] args) {
-        
+        // ^ Creamos la variable num la cual es la que va a ser comprobada si es un numero casi 0 o no
+        double num;
+
+        // ^ Creamos el Scanner, se lo asignamos a la variable read y cambiamos su formato al de US
+        final Scanner read = new Scanner(System.in);
+        read.useLocale(Locale.US);
+
+        // ! Pedimos al usuario que asigne un valor a la variable num
+        System.out.print("Introduce un numero, puede contener decimales: ");
+        num = read.nextDouble();
+
+        // ! Comprobamos mediante un if si nuestro numero esta entre -1 y 1
+        if (num != 0 && num > -1 && num < 1){
+            System.out.println(num + " es un numero casi 0");
+        }
+
+        else{
+            System.out.println(num + " no es un numero casi 0");
+        }
+
+        // ^ Cerramos el Scanner
+        read.close();
     }
 }
