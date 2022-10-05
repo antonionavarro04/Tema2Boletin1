ca968598504bdc93348d37281ddfc12c2cb12bd2
Antonio Navarro <anavarro41yt@gmail.com>
2022-10-05 14:40:38 +0200

    Ejercicio 6 Redo

--- src/ejercicio6/Ejercicio6.java
+++ src/ejercicio6/Ejercicio6.java
@@ -28,10 +28,12 @@ public class Ejercicio6 {
         // ? En caso de que 'a' sea 0 la ecuacion ya directamente no va a tener solucion, ya que cualquier numero dividido entre 0 no se puede calcular
         if (a == 0){
             System.out.println("La ecuacion no tiene solución Real");
+            System.out.println("Motivo: a = 0, no se puede dividir entre 0");
         } else{
             sqrt = Math.pow(b, 2) - (4 * a * c);
             if (sqrt < 0){
                 System.out.println("La ecuacion no tiene solución Real");
+                System.out.println("Motivo: No se puede hacer la raiz cuadrada de: " + sqrt);
             } else {
                 x1 = (-b + Math.sqrt(sqrt)) / (2 * a);
                 x2 = (-b - Math.sqrt(sqrt)) / (2 * a);
