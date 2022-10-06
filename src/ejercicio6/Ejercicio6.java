00c2d5471cbc91ba8be3455991fc3855898d36c8
Antonio Navarro <anavarro41yt@gmail.com>
2022-10-06 13:51:58 +0200

    Ejercicio 6 con numeros enteros en vez de Dobles

--- src/ejercicio6/Ejercicio6.java
+++ src/ejercicio6/Ejercicio6.java
@@ -7,7 +7,8 @@ import java.util.Locale;
 public class Ejercicio6 {
     public static void main(String[] args) {
         // ^ Creamos 3 variables; a, b y c las cuales van a ser usadas para hallar x y posteriormente resolver la ecuacion de segundo grado
-        double a, b, c, x1 = 0, x2 = 0, sqrt = 0;
+       long a, b, c;
+       double x1 = 0, x2 = 0, sqrt = 0;
 
         // ^ Creamos el Scanner, se lo asignamos a la variable read y cambiamos su formato al de US
         final Scanner read = new Scanner(System.in);
@@ -15,11 +16,11 @@ public class Ejercicio6 {
 
         // ! Pedimos al usuario que introudza los valores de a, b y c
         System.out.print("Introduce el valor de a: ");
-        a = read.nextDouble();
+        a = read.nextLong();
         System.out.print("Introduce el valor de b: ");
-        b = read.nextDouble();
+        b = read.nextLong();
         System.out.print("Introduce el valor de c: ");
-        c = read.nextDouble();
+        c = read.nextLong();
 
         // ? Salto de linea bonito
         System.out.println();System.out.println("---------------------------------------------");System.out.println();
